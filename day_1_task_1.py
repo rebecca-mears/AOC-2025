1d795ce6a261f0695e7fcd4b2bf4f4f3f2e7101a
rebecca-mears <rebecca-l.mears@virginmedia.com>
2025-12-02 00:28:37 +0000

    Added day 1 task 1

---
+++ day_1_task_1.py
@@ -0,0 +1,22 @@
+#%%
+with open("input.txt","r") as f:
+    input_string = f.read()
+
+input_string = input_string.replace("L", "-").replace("R", "")
+input = input_string.split("\n")
+input.pop()
+
+input_list = list(map(int, input))
+#%%
+
+def count_0s(input_list: list) -> int:
+    count = 0
+    dial = 50
+
+    for input in input_list:
+        dial += input
+
+        if dial % 100 == 0:
+            count += 1
+
+    return count
