755bc078f25f36cf20cdde91b079f6cfd824fb2e
rebecca-mears <rebecca-l.mears@virginmedia.com>
2025-12-10 01:35:38 +0000

    Task 1 complete

--- day_8/day_8_task_1.py
+++ day_8/day_8_task_1.py
@@ -1,5 +1,7 @@
 # %%
 from scipy.spatial.distance import pdist, squareform
+import pandas as pd
+import numpy as np
 
 # %%
 with open("input.txt", "r") as f:
@@ -7,7 +9,7 @@ with open("input.txt", "r") as f:
 
 
 # %%
-def get_product_3_largest(input_string):
+def get_product_3_largest_misunderstood(input_string):
     input_list = input_string.split("\n")
     input_list.pop()
 
@@ -39,6 +41,55 @@ def get_product_3_largest(input_string):
                 connections.pop(idx_list_index[0])
         else:
             connections[i_list_index[0]].append(closest_box_idx)
+
+    sorted_connections = sorted(connections, key=lambda item: len(item))
+
+    product = (
+        len(sorted_connections[-1])
+        * len(sorted_connections[-2])
+        * len(sorted_connections[-3])
+    )
+
+    return product
+
+
+# %%
+def get_product_3_largest(input_string):
+    input_list = input_string.split("\n")
+    input_list.pop()
+
+    input_vectors = list(map(lambda x: list(map(int, x.split(","))), input_list))
+
+    distance_matrix = squareform(pdist(input_vectors, metric="euclidean"))
+    distance_matrix_df = pd.DataFrame(distance_matrix).replace(0, np.nan)
+
+    connections = []
+    for _ in range(1000):
+        closest_boxes = distance_matrix_df.stack().idxmin()
+
+        if not any(closest_boxes[0] in connection for connection in connections):
+            connections.append([closest_boxes[0]])
+
+        list_idx_0 = [
+            index
+            for index, connection in enumerate(connections)
+            if closest_boxes[0] in connection
+        ]
+
+        if any(closest_boxes[1] in connection for connection in connections):
+            list_idx_1 = [
+                index
+                for index, connection in enumerate(connections)
+                if closest_boxes[1] in connection
+            ]
+            if list_idx_0[0] != list_idx_1[0]:
+                connections[list_idx_0[0]].extend(connections[list_idx_1[0]])
+                connections.pop(list_idx_1[0])
+        else:
+            connections[list_idx_0[0]].append(closest_boxes[1])
+
+        distance_matrix_df[closest_boxes[0]][closest_boxes[1]] = np.nan
+        distance_matrix_df[closest_boxes[1]][closest_boxes[0]] = np.nan
 
     sorted_connections = sorted(connections, key=lambda item: len(item))
 
